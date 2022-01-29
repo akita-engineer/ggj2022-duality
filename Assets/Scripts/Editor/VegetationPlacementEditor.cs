9c600506956c539a7c94efe405a09417417736e2
Vlad Ryzhov <akita.engineer@gmail.com>
2022-01-29 02:11:31 -0800

    Improve level editor

--- Assets/Scripts/Editor/VegetationPlacementEditor.cs
+++ Assets/Scripts/Editor/VegetationPlacementEditor.cs
@@ -32,42 +32,81 @@ public static class VegetationPlacementEditor
         InitializePlacement();
     }
 
-    static string grassPrefab = "Assets/Fantasy Forest Environment Free Sample/Meshes/Prefabs/grass01.prefab";
-    static string treesFolderPath = "Assets/Waldemarst/JapaneseGardenPackage/Prefabs/";
+    public class VegetationPlacementAssetsPointer
+    {
+        private string name;
+        public string Name => name;
+
+        private string path;
+        public string Path => path;
+
+        private string filter;
+        public string Filter => filter;
+
+        private bool placeColliders;
+        public bool PlaceColliders => placeColliders;
+
+        private Vector3 minScale;
+        public Vector3 MinScale => minScale;
+
+        private Vector3 maxScale;
+        public Vector3 MaxScale => maxScale;
+
+        public VegetationPlacementAssetsPointer(string name, string path, string filter, bool placeColliders, Vector3 minScale, Vector3 maxScale)
+        {
+            this.name = name;
+            this.path = path;
+            this.filter = filter;
+            this.placeColliders = placeColliders;
+            this.minScale = minScale;
+            this.maxScale = maxScale;
+        }
+    }
+
+    public static VegetationPlacementAssetsPointer[] placementObjects = new VegetationPlacementAssetsPointer[]
+    {
+        new VegetationPlacementAssetsPointer("Grass", "Assets/Fantasy Forest Environment Free Sample/Meshes/Prefabs/", "grass", false, Vector3.one, Vector3.one),
+        new VegetationPlacementAssetsPointer("Trees", "Assets/Waldemarst/JapaneseGardenPackage/Prefabs/", "Tree", true, Vector3.one / 1.5f, Vector3.one),
+        new VegetationPlacementAssetsPointer("Crystals", "Assets/SineVFX/TranslucentCrystals/Prefabs/", "Crystal", true, Vector3.one / 2.0f, Vector3.one),
+        new VegetationPlacementAssetsPointer("Rocks", "Assets/HQ_BigRock/", "Rock", true, Vector3.one / 2.0f, Vector3.one)
+    };
 
     private static void PlacementUpdate(SceneView scene)
     {
-       if (Event.current.isMouse && Event.current.button == 0 && Event.current.type == EventType.MouseDown)
+        if (Event.current.isScrollWheel && Event.current.shift)
         {
-            Debug.Log("Placing grass...");
-
-            Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hitInfo))
+            if (Event.current.delta.y < 0)
             {
-                GameObject grass = AssetDatabase.LoadAssetAtPath<GameObject>(grassPrefab);
+                int currentValue = EditorPrefs.GetInt("VegetationPlacementSelectedObject");
+                int nextValue = (currentValue + 1) % placementObjects.Length;
+                EditorPrefs.SetInt("VegetationPlacementSelectedObject", nextValue);
+                Debug.Log($"[PLACEMENT EDITOR] Switched to placing {placementObjects[nextValue].Name}");
+            }
 
-                GameObject grassRoot = GameObject.Find("Grass");
-                if (grassRoot == null)
-                {
-                    grassRoot = new GameObject("Grass");
-                }
-                GameObject.Instantiate(grass, hitInfo.point, Quaternion.identity, grassRoot.transform);
+            if (Event.current.delta.y > 0)
+            {
+                Debug.Log("Scroll down");
+                int currentValue = EditorPrefs.GetInt("VegetationPlacementSelectedObject");
+                int nextValue = currentValue == 0 ? placementObjects.Length - 1 : currentValue - 1;
+                EditorPrefs.SetInt("VegetationPlacementSelectedObject", nextValue);
+                Debug.Log($"[PLACEMENT EDITOR] Switched to placing {placementObjects[nextValue].Name}");
             }
         }
 
         if (Event.current.isMouse && Event.current.button == 2 && Event.current.type == EventType.MouseDown)
         {
-            Debug.Log("Placing a tree...");
+            VegetationPlacementAssetsPointer pointer = placementObjects[EditorPrefs.GetInt("VegetationPlacementSelectedObject")];
+
+            Debug.Log($"[PLACEMENT EDITOR] Placing {pointer.Name}...");
 
             Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit hitInfo))
             {
-                string[] assets = Directory.GetFiles(treesFolderPath);
-                List<GameObject> resultTreePool = new List<GameObject>();
+                string[] assets = Directory.GetFiles(pointer.Path);
+                List<GameObject> resultObjectPool = new List<GameObject>();
 
                 foreach (string assetPath in assets)
                 {
-                    Debug.Log(assetPath);
                     if (assetPath.Contains(".meta"))
                     {
                         continue;
@@ -78,23 +117,79 @@ public static class VegetationPlacementEditor
                         continue;
                     }
 
-                    if (!assetPath.Contains("Tree"))
+                    if (!assetPath.Contains(pointer.Filter))
                     {
                         continue;
                     }
 
-                    GameObject treePrefab = AssetDatabase.LoadAssetAtPath<GameObject>(assetPath);
-                    resultTreePool.Add(treePrefab);
+                    GameObject objectPrefab = AssetDatabase.LoadAssetAtPath<GameObject>(assetPath);
+                    resultObjectPool.Add(objectPrefab);
                 }
 
-                GameObject randomTree = resultTreePool[Random.Range(0, resultTreePool.Count - 1)];
+                GameObject randomObject = resultObjectPool[Random.Range(0, resultObjectPool.Count - 1)];
+                GameObject placementRoot = null;
 
-                GameObject treeRoot = GameObject.Find("Trees");
-                if (treeRoot == null)
+                // We'd want to keep objects grouped on islands
+                FloatingIsland islandRoot = hitInfo.transform.gameObject.GetComponentInParent<FloatingIsland>();
+                if (islandRoot == null)
                 {
-                    treeRoot = new GameObject("Trees");
+                    placementRoot = GameObject.Find(pointer.Name);
+                    if (placementRoot == null)
+                    {
+                        placementRoot = new GameObject(pointer.Name);
+                        placementRoot.isStatic = true;
+                    }
                 }
-                GameObject.Instantiate(randomTree, hitInfo.point, Quaternion.identity, treeRoot.transform);
+                else
+                {
+                    for (int i = 0; i < islandRoot.transform.childCount; i++)
+                    {
+                        GameObject child = islandRoot.transform.GetChild(i).gameObject;
+                        if (child.name == pointer.Name)
+                        {
+                            placementRoot = child;
+                            break;
+                        }
+                    }
+
+                    if (placementRoot == null)
+                    {
+                        placementRoot = new GameObject(pointer.Name);
+                        placementRoot.isStatic = true;
+                        placementRoot.transform.parent = islandRoot.transform;
+                        placementRoot.transform.localPosition = Vector3.zero;
+
+                    }
+                }
+
+                GameObject result = GameObject.Instantiate(randomObject, hitInfo.point, Quaternion.identity, placementRoot.transform);
+
+                // Static
+                foreach (Transform t in result.GetComponentsInChildren<Transform>())
+                {
+                    t.gameObject.isStatic = true;
+                }
+
+                // Scale
+                float randomScale = Random.Range(0.0f, 1.0f);
+                result.transform.localScale = Vector3.Lerp(pointer.MinScale, pointer.MaxScale, randomScale);
+
+                // Colliders
+                if (pointer.PlaceColliders)
+                {
+                    Collider[] colliders = result.GetComponentsInChildren<Collider>();
+                    if (colliders.Length == 0)
+                    {
+                        // Need to add mesh colliders if NO colliders are present
+                        Renderer[] allRenderers = result.GetComponentsInChildren<Renderer>();
+                        foreach (Renderer renderObject in allRenderers)
+                        {
+                            renderObject.gameObject.AddComponent<MeshCollider>();
+                        }
+                    }
+                }
+
+                Undo.RegisterCreatedObjectUndo(result, "Vegetation Placement Spawn");
             }
         }
     }
